f5bfe3c5f7c5233b251f5639903ee2d0759556fa
Pavel Senin <seninp@gmail.com>
2015-10-02 13:33:49 +0200

    adding znorm

---
+++ R/jmotif.R
@@ -0,0 +1,55 @@
+# Hello, world!
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+znorm <- function(ts, threshold){
+
+  if(is.matrix(ts)){
+
+    # check dimensions
+    if(1 == dim(ts)[1]){
+     ts_sd = sd(ts[1,])
+      if(ts_sd<threshold){
+        return (unlist(ts[1,], use.names=F))
+      }else{
+        return (unlist((ts[1,] - mean(ts[1,]))/ts_sd, use.names=F))
+      }
+
+    } else if(1 == dim(ts)[2]){
+      ts_sd = sd(ts[,1])
+      if(ts_sd<threshold){
+        return (unlist(ts[,1], use.names=F))
+      }else{
+        return (unlist((ts[,1] - mean(ts[,1]))/ts_sd, use.names=F))
+      }
+
+    } else {
+      stop(paste("don't know how to process matrix of dimension",dim(ts)))
+    }
+
+  }else if (is.vector(ts)){
+    ts_sd = sd(ts)
+    if(ts_sd<threshold){
+      return(ts)
+    }else{
+      return (unlist((ts - mean(ts))/ts_sd, use.names=F))
+    }
+
+
+  } else {
+    stop(sprintf("argument %s must be matrix or array", sQuote("ts")))
+  }
+
+}
+
+#library(jmotif)
+#x=seq(0,pi*4,0.02)
+#y=sin(x)*5+rnorm(length(x))
+#plot(x,y,type="l",col="blue")
+#lines(x,znorm(y,0.01),type="l",col="red")
+#dim(y)<-c(1,2)
+#lines(x,znorm(y,0.01),type="l",col="green")
+#y=t(t(y))
+#lines(x,znorm(y,0.01),type="l",col="orange")
