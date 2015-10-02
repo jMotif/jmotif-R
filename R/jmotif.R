c985b35d6d7106f59ee35c739b1ba6cd576f4879
Pavel Senin <seninp@gmail.com>
2015-10-02 16:15:48 +0200

    adding some functionality

--- R/jmotif.R
+++ R/jmotif.R
@@ -4,6 +4,87 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+##
+## Converts the specified resolution into the cut points
+##
+alphabet2cut <- function(a_size){
+  if(a_size>20 && a_size < 1){
+    stop(paste("unable to get alphabet cuts for the alphabet size",a_size))
+  }
+  switch(a_size,
+    0.00, #2
+    c(-Inf,  0.00), #3
+    c(-Inf, -0.43,  0.43), #4
+    c(-Inf, -0.67,  0.00,  0.67), #5
+    c(-Inf, -0.84, -0.25,  0.25,  0.84), #6
+    c(-Inf, -0.97, -0.43,  0.00,  0.43,  0.97), #7
+    c(-Inf, -1.07, -0.57, -0.18,  0.18,  0.57,  1.07), #8
+    c(-Inf, -1.15, -0.67, -0.32,  0.00,  0.32,  0.67,  1.15), #9
+    c(-Inf, -1.22, -0.76, -0.43, -0.14,  0.14,  0.43,  0.76,  1.22), #10
+    c(-Inf, -1.28, -0.84, -0.52, -0.25,  0.00,  0.25,  0.52,  0.84,  1.28), #11
+    c(-Inf, -1.34, -0.91, -0.60, -0.35, -0.11,  0.11,  0.35,  0.60,  0.91, 1.34), #12
+    c(-Inf, -1.38, -0.97, -0.67, -0.43, -0.21,  0.00,  0.21,  0.43,  0.67, 0.97, 1.38), #13
+    c(-Inf, -1.43, -1.02, -0.74, -0.50, -0.29, -0.10,  0.10,  0.29,  0.50, 0.74, 1.02, 1.43), #14
+    c(-Inf, -1.47, -1.07, -0.79, -0.57, -0.37, -0.18,  0.00,  0.18,  0.37, 0.57, 0.79, 1.07, 1.47), #15
+    c(-Inf, -1.50, -1.11, -0.84, -0.62, -0.43, -0.25, -0.08,  0.08,  0.25, 0.43, 0.62, 0.84, 1.11, 1.5), #16
+    c(-Inf, -1.53, -1.15, -0.89, -0.67, -0.49, -0.32, -0.16,  0.00,  0.16, 0.32, 0.49, 0.67, 0.89, 1.15, 1.53), #17
+    c(-Inf, -1.56, -1.19, -0.93, -0.72, -0.54, -0.38, -0.22, -0.07,  0.07, 0.22, 0.38, 0.54, 0.72, 0.93, 1.19, 1.56), #18
+    c(-Inf, -1.59, -1.22, -0.97, -0.76, -0.59, -0.43, -0.28, -0.14,  0.00, 0.14, 0.28, 0.43, 0.59, 0.76, 0.97, 1.22, 1.59), #19
+    c(-Inf, -1.62, -1.25, -1.00, -0.80, -0.63, -0.48, -0.34, -0.20, -0.07, 0.07, 0.20, 0.34, 0.48, 0.63, 0.80, 1.00, 1.25, 1.62), #20
+    c(-Inf, -1.64, -1.28, -1.04, -0.84, -0.67, -0.52, -0.39, -0.25, -0.13, 0.00, 0.13, 0.25, 0.39, 0.52, 0.67, 0.84, 1.04, 1.28, 1.64) #21
+  )
+}
+
+
+##
+## compute distance matrix for the alphabet size specified
+##
+distance_matrix <- function (alphabet_size){
+  if(alphabet_size>1 && alphabet_size<20){
+    cutlines <- alphabet2cut(alphabet_size)[2:alphabet_size]
+    distance_matrix <- matrix(rep(0, alphabet_size*alphabet_size), byrow=T, nrow=alphabet_size, ncol=alphabet_size)
+    i=1
+    while(i <= alphabet_size){
+      # the min_dist for adjacent symbols are 0, so we start with i+2
+      j=i+2;
+      while(j <= alphabet_size){
+        # square the distance now for future use
+        distance_matrix[i,j]=(cutlines[i]-cutlines[j-1])*(cutlines[i]-cutlines[j-1])
+        # the distance matrix is symmetric
+        distance_matrix[j,i] = distance_matrix[i,j]
+        j=j+1;
+      }
+      i=i+1;
+    }
+    distance_matrix
+  }
+}
+
+##
+## Converts the specified resolution into the cut points
+##
+num2letter <- function(num){
+  letters <- c("a",  "b",  "c",  "d",  "e",
+               "f",  "g",  "h",  "i",  "j",
+               "k",  "l",  "m",  "n",  "o",
+               "p",  "q",  "r",  "s",  "t",
+               "u",  "v",  "w",  "x",  "y",  "z")
+  letters[num]
+}
+
+
+##
+## Converts the timeseries into string
+##
+ts2string <- function(ts, aSize){
+  cut_points <- alphabet2cut(aSize)
+  res <- rep(0, ncol(ts))
+  for(i in 1:ncol(ts)){
+    res[i] = length(cut_points[cut_points<=ts[i]])
+  }
+  num2letter(res)
+}
+
 znorm <- function(ts, threshold=0.01){
 
   if(is.matrix(ts)){
