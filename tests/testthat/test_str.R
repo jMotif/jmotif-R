9fa0701438d1301d881752a4512bb6ddd97b7d65
seninp <seninp@gmail.com>
2015-10-03 13:37:12 +0200

    testing the build

--- tests/testthat/test_str.R
+++ tests/testthat/test_str.R
@@ -1,7 +1,10 @@
 test_that("idx to letter", {
   expect_true(is.character(idx2letter(3)))
   expect_equal(idx2letter(3), "c")
+})
+
+test_that("letter to idx", {
   expect_true(is.numeric(letter2idx('c')))
   expect_equal(letter2idx('c'), 3)
-
 })
+
