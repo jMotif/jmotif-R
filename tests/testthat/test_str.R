51ea9148945aa3d6bf54d63a303dc01bd02fa88f
seninp <seninp@gmail.com>
2015-10-03 13:18:18 +0200

    adding coverall report

--- tests/testthat/test_str.R
+++ tests/testthat/test_str.R
@@ -1,4 +1,7 @@
 test_that("idx to letter", {
   expect_true(is.character(idx2letter(3)))
   expect_equal(idx2letter(3), "c")
+  expect_true(is.numeric(letter2idx('c')))
+  expect_equal(letter2idx('c'), 3)
+
 })
