5007e635df2cc726c401a2992f00fef87cb05418
seninp <seninp@gmail.com>
2015-10-04 21:03:11 +0200

    working on sliding window

---
+++ assets/sliding_tinker.R
@@ -0,0 +1,23 @@
+x = seq(0, pi*4, 0.02)
+ts = sin(x) * 5 + rnorm(length(x))
+paa_size=5
+alphabet_size=3
+window_size=10
+n_threshold=0.01
+
+library(plyr)
+ts2sax_via_window <- function(ts, paa_size, alphabet_size, window_size, n_threshold){
+
+  res = list()
+  for(i in c(1:(length(ts-window_length)))){
+    tmp = ts[i : (i + window_size)]
+    tmp_znorm = znorm(tmp, n_threshold)
+    tmp_paa = paa(tmp_znorm, paa_size)
+    tmp_sax = paste(ts2string(tmp_paa, alphabet_size), sep = "", collapse = "")
+    tmp_sax
+  }
+ for(e : raw_list){
+   print(e)
+ }
+}
+
