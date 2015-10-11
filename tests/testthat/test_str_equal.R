970a51cb45c85f3225e8a9af6e2b1faa0814683a
seninp <seninp@gmail.com>
2015-10-11 18:10:02 +0200

    fixing the failing test

--- tests/testthat/test_str_equal.R
+++ tests/testthat/test_str_equal.R
@@ -5,5 +5,5 @@ test_that("test str equality", {
 
   expect_false(is_equal_str("asfwd", "asfw"))
   expect_false(is_equal_str("", "-1"))
-  expect_true(is_equal_str("asfw", "atfw"))
+  expect_false(is_equal_str("asfw", "atfw"))
 })
