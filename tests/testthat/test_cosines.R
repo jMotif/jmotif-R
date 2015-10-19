59d2561636d03671d355d52ecf0bc534dcec19ec
Pavel Senin <seninp@gmail.com>
2015-10-19 10:29:23 +0200

    fixing lintr warnings [ci skip]

--- tests/testthat/test_cosines.R
+++ tests/testthat/test_cosines.R
@@ -8,13 +8,13 @@ bag2 <- manyseries_to_wordbag(CBF[["data_train"]][CBF[["labels_train"]] == 2,],
 bag3 <- manyseries_to_wordbag(CBF[["data_train"]][CBF[["labels_train"]] == 3,],
                               w, p, a, "exact", 0.01)
 
-sample = (CBF[["data_test"]][CBF[["labels_test"]] == 3,])[1,]
+sample <- (CBF[["data_test"]][CBF[["labels_test"]] == 3,])[1,]
 
 test_that("test cosine sim #1", {
 
-  tfidf = bags_to_tfidf( list("cylinder" = bag1, "bell" = bag2, "funnel" = bag3) )
-  bag = series_to_wordbag(sample, w, p, a, "exact", 0.01)
-  cosines = cosine_sim(list("bag"=bag, "tfidf" = tfidf))
+  tfidf <- bags_to_tfidf( list("cylinder" = bag1, "bell" = bag2, "funnel" = bag3) )
+  bag <- series_to_wordbag(sample, w, p, a, "exact", 0.01)
+  cosines <- cosine_sim(list("bag" = bag, "tfidf" = tfidf))
   expect_equal(3, which(cosines$cosines == max(cosines$cosines)))
 
 })
